23b7027cba8b7dad353ddffa0eb3e759ac468a80
Alisa Promthep <alisa.promthep@gmail.com>
2022-07-12 13:43:05 -0400

    created JSON file for food and symptoms icon, and refracted the assets route to read and send back file from the json file

--- routes/publicRoute.js
+++ routes/publicRoute.js
@@ -1,26 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const knexfile = require('../knexfile').development;
-const knex = require('knex')(knexfile);
+const {readFile} = require('../utility/readAndWrite');
 
 //get all food icons for the form 
 router.get("/food", (_req,res)=>{
-    knex
-        .from('food_info')
-        .select('id', 'name', 'img_file')
-        .then((data)=>{
-            res.status(200).json(data)
-        })
+    const foodIconFile = readFile('./data/foodImg.json');
+    res.status(200).json(foodIconFile);
+
 })
 
 //get all the symptoms icons for the form 
 router.get("/symptoms", (_req,res)=>{
-    knex
-        .from('symptom_info')
-        .select('id', 'name', 'img_file')
-        .then((data)=>{
-            res.status(200).json(data)
-        })
+    const foodIconFile = readFile('./data/symptomsImg.json');
+    res.status(200).json(foodIconFile);
 })
 
 
